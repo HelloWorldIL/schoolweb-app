5fdfd058ab98309abccc94858aa074667f9e175f
Ido Bronfeld <idobronfeld@gmail.com>
2019-06-13 21:38:16 +0300

    fixes multiple calls to auth/setLoaded

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -3,9 +3,12 @@ import { auth } from '~/services/firebaseinit.js'
 export default ({ store }) => {
   return new Promise(resolve => {
     auth().onAuthStateChanged(function(user) {
-      store.commit('auth/setLoaded', true)
+      if (store.getters['auth/isLoaded'] === false) {
+        store.commit('auth/setLoaded', true)
+      }
       if (user) {
         return resolve(
+          // JSON actions to deep copy object
           store.commit('auth/setUser', JSON.parse(JSON.stringify(user)))
         )
       }
