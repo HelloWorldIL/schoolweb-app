66202fe7c0611b8d16d181e25916d52c101bfbd5
Ido Bronfeld <idobronfeld@gmail.com>
2019-06-18 23:48:01 +0300

    added basic about page text

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,18 @@
 <template lang="pug">
   v-card
-    v-card-title(priamry-title)
-      h1 xd
+    v-card-title(primary-title)
+      div
+        .headline HSL
+        span {{about_text}}
+    v-img(src="/sat.jpg")
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      about_text: 'This website allows you to manage a database of satellites.'
+    }
+  }
+}
+</script>
