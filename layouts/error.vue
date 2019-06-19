49357344ad6c602218e9e590a9e0e6e361b2f555
Ido Bronfeld <idobronfeld@gmail.com>
2019-06-19 06:03:21 +0300

    404 page cahanges

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,5 +1,10 @@
 <template lang="pug">
   v-card
-    v-card-title
-      h1 404 not found
+    v-img(:src="require('@/assets/blackHole.png')" height="600px" width="500px")
+      v-container(fluid fill-height pt-5)
+        v-layout(column)
+          v-flex(xs12 class="text-xs-center")
+            span(class="headline white--text") 404 PAGE NOT FOUND
+          v-flex(xs2 class="text-xs-bottom")
+            span(class="white--text") Be carefull not to fall down the event horizon!
 </template>
